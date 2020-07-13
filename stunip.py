5c1f454f08d65dad223624d7db03a06d46a67b66
Brian Rzycki <brzycki@gmail.com>
2020-07-13 08:55:04 -0500

    WireFormat: Only call reset implicitly when there is no id.
    Gives the user of the WireFormat more control on when reset is
    called.
    
    Also converted all single quotes to double quotes for code
    consistency.

--- stunip.py
+++ stunip.py
@@ -19,14 +19,15 @@ class WireFormat(object):
     "https://tools.ietf.org/html/rfc3489"
     def reset(self):
         self.id = uuid.uuid4().bytes
-        self.ip = ''
+        self.ip = ""
 
     def request(self):
         # A STUN header contains a Message Type, Message Length, and
         # a Transaction ID. The shortest and simplest request type is
         # a Binding Request (0x0001) with a Message Length of 0.
-        self.reset()
-        return struct.pack('!HH16s', 0x0001, 0, self.id)
+        if not getattr(self, "id", ""):
+            self.reset()
+        return struct.pack("!HH16s", 0x0001, 0, self.id)
 
     def response(self, buf, cached=True):
         if cached and self.ip:
@@ -53,7 +54,7 @@ class WireFormat(object):
         # A Binding Response (0x0101) is the only valid STUN packet for the
         # Binding Request we sent. The Transaction ID must match the ID
         # sent in the Binding Request.
-        stun_type, stun_len, stun_id = struct.unpack('!HH16s', buf[:20])
+        stun_type, stun_len, stun_id = struct.unpack("!HH16s", buf[:20])
         if stun_type != 0x0101 or stun_id != self.id:
             return False
 
@@ -73,7 +74,7 @@ class WireFormat(object):
         # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
         # A Binding Response has only one valid (and mandatory) Attribute:
         # MAPPED-ADDRESS (0x0001) with a fixed Value Length of 8 bytes.
-        attr_type, value_len = struct.unpack('!HH', attrs[:4])
+        attr_type, value_len = struct.unpack("!HH", attrs[:4])
         if attr_type != 0x0001 or value_len != 8:
             return False
         value = attrs[4:4+value_len]
@@ -87,7 +88,7 @@ class WireFormat(object):
         # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
         # The Alignment byte and the Port in a MAPPED-ADDRESS Value are
         # ignored for our use-case. The RFC states Family is always 1.
-        _, family, _, address = struct.unpack('!BBH4s', value)
+        _, family, _, address = struct.unpack("!BBH4s", value)
         if family != 1:
             return False
 
@@ -102,7 +103,7 @@ class WireFormat(object):
 
 
 class StunIP(object):
-    def __init__(self, saddr='0.0.0.0', sport=0, timeout=0.5, max_tries=10):
+    def __init__(self, saddr="0.0.0.0", sport=0, timeout=0.5, max_tries=10):
         self.max_tries = max_tries
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -118,36 +119,36 @@ class StunIP(object):
                     return w.ip
             except socket.timeout:
                 pass
-        return ''
+        return ""
 
 
 def main():
     import argparse
     p = argparse.ArgumentParser(
-        description='Fetch external IPv4 address using STUN',
+        description="Fetch external IPv4 address using STUN",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    p.add_argument('-a', '--address', default='0.0.0.0:0',
-                   help='source address[:port]')
-    p.add_argument('-m', '--max-tries', default=10, type=int,
-                   help='max tries to send a STUN request')
-    p.add_argument('-t', '--timeout', default=0.5, type=float,
-                   help='socket timeout (in seconds) per request attempt')
-    p.add_argument('--version', action='version',
-                   version='%%(prog)s %s-%s' % (__version__, __status__))
-    p.add_argument('server', default='stun.stunprotocol.org:3478', nargs='?',
-                   help='STUN server[:port]')
+    p.add_argument("-a", "--address", default="0.0.0.0:0",
+                   help="source address[:port]")
+    p.add_argument("-m", "--max-tries", default=10, type=int,
+                   help="max tries to send a STUN request")
+    p.add_argument("-t", "--timeout", default=0.5, type=float,
+                   help="socket timeout (in seconds) per request attempt")
+    p.add_argument("--version", action="version",
+                   version="%%(prog)s %s-%s" % (__version__, __status__))
+    p.add_argument("server", default="stun.stunprotocol.org:3478", nargs="?",
+                   help="STUN server[:port]")
     a = p.parse_args()
 
     saddr, sport = (a.address, 0)
-    if ':' in a.address:
-        saddr, sport = a.address.split(':')[:2]
+    if ":" in a.address:
+        saddr, sport = a.address.split(":")[:2]
         try:
             sport = int(sport)
         except ValueError:
             p.error("invalid source port '%s'" % dport)
     daddr, dport = (a.server, 3478)
-    if ':' in a.server:
-        daddr, dport = a.server.split(':')[:2]
+    if ":" in a.server:
+        daddr, dport = a.server.split(":")[:2]
         try:
             dport = int(dport)
         except:
@@ -161,7 +162,7 @@ def main():
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Suppress broken pipe exceptions.
     from signal import signal, SIGPIPE, SIG_DFL
     signal(SIGPIPE, SIG_DFL)
